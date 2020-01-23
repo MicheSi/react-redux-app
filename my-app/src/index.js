8535ae9d89d77c3fbf53b07866624b2c05ce1dda
mlazzell17@gmail.com <MicheSi>
2020-01-23 13:19:15 -0700

    set up store and reducer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import {reducer} from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ my-app/src/reducers/index.js
@@ -0,0 +1,17 @@
+
+
+const initialState = {
+    isLoading: false,
+    error: '',
+    character: '',
+    image: '',
+    name: '',
+    amiiboSeries: ''
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
